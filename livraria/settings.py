6f7c54d5832f9fa3c286fee72c30e69e5bb599ff
Lizie <gabbmaia2004@gmail.com>
2022-10-20 14:09:44 -0300

    Configurando variáveis de ambiente

--- livraria/settings.py
+++ livraria/settings.py
@@ -10,12 +10,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(u#r39$e5no9xcsrb-10(sc!5^n&610q^b0xqx6ter3cp*ndx+'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
